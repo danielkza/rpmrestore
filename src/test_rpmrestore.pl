a5fe5300ce5a85180240f577534673f6ea03dc96
gerbier <gerbier@9616081a-d31e-0410-93ec-824d99299f1c>
2007-03-19 12:26:12 +0000

    use $cmd to store rpmrestore command for all tests

--- src/test_rpmrestore.pl
+++ src/test_rpmrestore.pl
@@ -25,37 +25,38 @@ use Test::More qw(no_plan);
 use Data::Dumper;
 
 # arguments test
+my $cmd = './rpmrestore.pl';
 
 # 1 no arguments
-my $out = `./rpmrestore.pl 2>&1`;
+my $out = `$cmd 2>&1`;
 like( $out, qr/missing rpm package name/, 'no parameter' );
 
 # 2 version
-$out = `./rpmrestore.pl --version 2>&1`;
+$out = `$cmd --version 2>&1`;
 like( $out, qr/rpmrestore\.pl version/, 'version' );
 
 # 3 help
-$out = `./rpmrestore.pl --help`;
+$out = `$cmd --help`;
 like( $out, qr/Usage:/, 'help' );
 
 # 4 man
-$out = `./rpmrestore.pl --man`;
+$out = `$cmd --man`;
 like( $out, qr/User Contributed Perl Documentation/, 'man' );
 
 # 5 bad package
-$out = `./rpmrestore.pl -p tototo 2>&1`;
+$out = `$cmd -p tototo 2>&1`;
 like( $out, qr/package does not exists/, 'no package' );
 
 # 6 bad file name
-$out = `./rpmrestore.pl -f tototo 2>&1`;
+$out = `$cmd -f tototo 2>&1`;
 like( $out, qr/can not find .* file/, 'no file' );
 
 # 7 file not from package
-$out = `./rpmrestore.pl -f Todo 2>&1`;
+$out = `$cmd -f Todo 2>&1`;
 like( $out, qr/is not owned by any package/, 'not from rpm' );
 
 # 8 no changes on package
-$out = `./rpmrestore.pl -p rpmrestore 2>&1`;
+$out = `$cmd -p rpmrestore 2>&1`;
 like( $out, qr/0 changes detected/, 'package no changes' );
 
 # we work on rpm package, so the rpm should exist
@@ -63,7 +64,7 @@ my $file = '/bin/rpm';
 if ( -e $file ) {
 
 	# 9 no changes on file
-	$out = `./rpmrestore.pl  -n -f $file 2>&1`;
+	$out = `$cmd  -n -f $file 2>&1`;
 	like( $out, qr/0 changes detected/, 'file no changes' );
 }
 else {
@@ -78,38 +79,38 @@ if ( $> == 0 ) {
 	system("touch $file");
 
 	# 10 no changes on attribute
-	$out = `./rpmrestore.pl -mode -n -f $file 2>&1`;
+	$out = `$cmd -mode -n -f $file 2>&1`;
 	like( $out, qr/0 changes detected/, 'attribute no changes' );
 
 	# 11 test attribute change
-	$out = `./rpmrestore.pl -t -n -f $file 2>&1`;
+	$out = `$cmd -t -n -f $file 2>&1`;
 	like( $out, qr/$file mtime orig/, 'attribute changes' );
 
 	my $log = 'log';
 	unlink $log if ( -e $log );
 
 	# 12 restore attribute change
-	$out = `./rpmrestore.pl -t -b -f $file -log $log 2>&1`;
+	$out = `$cmd -t -b -f $file -log $log 2>&1`;
 	like( $out, qr/change mtime on $file/, 'restore attribute' );
 
 	# 13 check restore
-	$out = `./rpmrestore.pl -t -n -f $file 2>&1`;
+	$out = `$cmd -t -n -f $file 2>&1`;
 	like( $out, qr/0 changes detected/, 'check restore' );
 
 	# 14 rollback 1
-	$out = `./rpmrestore.pl -mode -b -r $log 2>&1`;
+	$out = `$cmd -mode -b -r $log 2>&1`;
 	like( $out, qr/rollback 0 attributes/, 'no rollback' );
 
 	# 15 rollback 2
-	$out = `./rpmrestore.pl -t -b -r $log 2>&1`;
+	$out = `$cmd -t -b -r $log 2>&1`;
 	like( $out, qr/rollback 1 attributes/, 'rollback attribute' );
 
 	# 16 interactive change
-	$out = `echo 'n' | ./rpmrestore.pl -f $file  2>&1`;
+	$out = `echo 'n' | $cmd -f $file  2>&1`;
 	like( $out, qr/0 changes applied/, 'interactive no changes' );
 
 	# 17 interactive change
-	$out = `echo 'y' | ./rpmrestore.pl -f $file  2>&1`;
+	$out = `echo 'y' | $cmd -f $file  2>&1`;
 	like( $out, qr/1 changes applied/, 'interactive changes' );
 
 	unlink $log;
