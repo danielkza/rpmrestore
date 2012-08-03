b7f101b4b10c9a5c5c4540719c1cb6cf5a6617e3
Eric Gerbier <gerbier@users.sourceforge.net>
2012-08-03 09:25:10 +0200

    add tests for new syntax

--- src/test_rpmrestore.pl
+++ src/test_rpmrestore.pl
@@ -21,9 +21,11 @@
 
 use strict;
 use warnings;
+use English qw(-no_match_vars);
 use Test::More qw(no_plan);
 use Data::Dumper;
 
+## no critic (ProhibitBacktickOperators)
 ###############################################################################
 sub search_command($) {
 	my $prog = shift @_;
@@ -36,6 +38,9 @@ sub search_command($) {
 	return 0;
 }
 ###############################################################################
+# rem some tests are made on rpmrestore packages / files
+my $file = '/usr/bin/rpmrestore';
+
 # arguments test
 my $cmd = './rpmrestore.pl';
 
@@ -55,73 +60,84 @@ like( $out, qr/Usage:/, 'help' );
 $out = `$cmd --man`;
 like( $out, qr/User Contributed Perl Documentation/, 'man' );
 
+# old deprecated syntaxe
+########################
 # 5 bad package
 $out = `$cmd -p tototo 2>&1`;
-like( $out, qr/package does not exists/, 'no package' );
+like( $out, qr/package does not exists/, 'no package (deprecated)' );
 
 # 6 bad file name
 $out = `$cmd -f tototo 2>&1`;
-like( $out, qr/is not a valid file/, 'no file' );
+like( $out, qr/is not a valid file/, 'no file (deprecated)' );
 
 # 7 file not from package
 $out = `$cmd -f Todo 2>&1`;
-like( $out, qr/is not owned by any package/, 'not from rpm' );
+like( $out, qr/is not owned by any package/, 'not from rpm (deprecated)' );
 
 # 8 no changes on package
+$out = `$cmd -p rpmrestore 2>&1`;
+like( $out, qr/0 changes detected/, 'package no changes (deprecated)' );
+
+# 9 no changes on file
+$out = `$cmd -f $file 2>&1`;
+like( $out, qr/0 changes detected/, 'file no changes (deprecated)' );
+
+# new syntax
+############
+# 10 bad package
+$out = `$cmd tototo 2>&1`;
+like( $out, qr/package does not exists/, 'no package (new)' );
+
+# 11 file not from package
+$out = `$cmd Todo 2>&1`;
+like( $out, qr/is not owned by any package/, 'not from rpm (new)' );
+
+# 12 no changes on package
 $out = `$cmd rpmrestore 2>&1`;
-like( $out, qr/0 changes detected/, 'package no changes' );
+like( $out, qr/0 changes detected/, 'package no changes (new)' );
 
-# we work on rpm package, so the rpm should exist
-my $file = '/bin/rpm';
-if ( -e $file ) {
-
-	# 9 no changes on file
-	$out = `$cmd  -n -f $file 2>&1`;
-	like( $out, qr/0 changes detected/, 'file no changes' );
-}
-else {
-	diag("can not find $file file");
-	exit;
-}
+# 13 no changes on file
+$out = `$cmd $file 2>&1`;
+like( $out, qr/0 changes detected/, 'file no changes (new)' );
 
 # change only on root user
-if ( $> == 0 ) {
+if ( $EFFECTIVE_USER_ID == 0 ) {
 
 	# change mtime
 	system("touch $file");
 
-	# 10 no changes on attribute
+	# 14 no changes on attribute
 	$out = `$cmd -mode -n -f $file 2>&1`;
 	like( $out, qr/0 changes detected/, 'attribute no changes' );
 
-	# 11 test attribute change
+	# 15 test attribute change
 	$out = `$cmd -t -n -f $file 2>&1`;
 	like( $out, qr/$file mtime orig/, 'attribute changes' );
 
 	my $log = 'log';
 	unlink $log if ( -e $log );
 
-	# 12 restore attribute change
+	# 16 restore attribute change
 	$out = `$cmd -t -b -f $file -log $log 2>&1`;
 	like( $out, qr/change mtime on $file/, 'restore attribute' );
 
-	# 13 check restore
+	# 17 check restore
 	$out = `$cmd -t -n -f $file 2>&1`;
 	like( $out, qr/0 changes detected/, 'check restore' );
 
-	# 14 rollback 1
+	# 18 rollback 1
 	$out = `$cmd -mode -b -r $log 2>&1`;
 	like( $out, qr/rollback 0 attributes/, 'no rollback' );
 
-	# 15 rollback 2
+	# 19 rollback 2
 	$out = `$cmd -t -b -r $log 2>&1`;
 	like( $out, qr/rollback 1 attributes/, 'rollback attribute' );
 
-	# 16 interactive change
+	# 20 interactive change
 	$out = `echo 'n' | $cmd -f $file  2>&1`;
 	like( $out, qr/0 changes applied/, 'interactive no changes' );
 
-	# 17 interactive change
+	# 21 interactive change
 	$out = `echo 'y' | $cmd -f $file  2>&1`;
 	like( $out, qr/1 changes applied/, 'interactive changes' );
 
@@ -131,28 +147,27 @@ if ( $> == 0 ) {
 	# test for getcap
 	my $getcap = search_command('setcap');
 	if ($getcap) {
+
 		# will work on ping
 		my $filecap = '/usr/bin/ping';
 
-		# no changes
+		# 22 no changes
 		$out = `$cmd -capability -n -f $filecap 2>&1`;
 		like( $out, qr/0 changes detected/, 'capability no changes' );
 
-		# remove capability
+		# 23 remove capability
 		system "setcap -r $filecap";
 		$out = `$cmd -capability -n -f $filecap 2>&1`;
 		like( $out, qr/$filecap capability orig/, 'capability changes' );
 
-		# restore
+		# 24 restore
 		$out = `$cmd -capability -b -f $filecap 2>&1`;
 		like( $out, qr/change capability on $filecap/, 'restore capability' );
 
-		# no changes
+		# 25 no changes
 		$out = `$cmd -capability -n -f $filecap 2>&1`;
 		like( $out, qr/0 changes detected/, 'capability no changes' );
 	}
-
-
 }
 else {
 	diag('you should be root to run other tests');
@@ -186,7 +201,7 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-=head1 AUTHORS
+=head1 AUTHOR
 
 Eric Gerbier
 
